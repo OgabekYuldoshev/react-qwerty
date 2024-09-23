0414445105a1a165af5ab8952d5c659acf6db8db
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-09-23 10:35:04 +0500

    feat(custom): ✨ create new tndent extention

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -1,9 +1,11 @@
 import Placeholder from "@tiptap/extension-placeholder";
 import StarterKit from "@tiptap/starter-kit";
+import { Indent } from "./Indent";
 
 export const defaultExtension = [
 	StarterKit,
 	Placeholder.configure({
 		placeholder: "Write something …",
 	}),
+	Indent,
 ];
